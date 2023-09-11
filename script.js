a38d6dd8e84855821e3f9805e6d4b4abae06f74f
Karina Sasaki <karinasasaki99@gmail.com>
2023-09-11 03:38:36 -0300

    Adiciona JS troca de fotos

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function toggleMode() {
   const html = document.documentElement;
-  
-  html.classList.toggle("light");
+  const img = document.querySelector(".profile img")
+
+  if(html.classList.contains("light")) {
+    html.classList.remove("light");
+    html.classList.add("dark");
+    img.setAttribute("src", "assets/profile-pic-dark.jpg")
+    img.setAttribute("alt", "Foto de Karina Sasaki de lado sorrindo, segurando a ponta do cabelo, usando óculos e camiseta cinza.")
+  } else {
+    html.classList.add("light");
+    html.classList.remove("dark");
+    img.setAttribute("src", "assets/profile-pic-light.jpg")
+    img.setAttribute("alt", "Foto de rosto de Karina Sasaki, usando óculos.")
+  }
 }
