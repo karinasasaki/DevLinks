65da81a733c5505a8fdaec612393d6a587af52c9
Karina Sasaki <karinasasaki99@gmail.com>
2023-09-11 12:04:15 -0300

    Bg muda com media queries

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function toggleMode() {
   if(html.classList.contains("light")) {
     html.classList.remove("light");
     html.classList.add("dark");
-    img.setAttribute("src", "assets/profile-pic-dark.jpg")
+    img.setAttribute("src", "assets/profile-pic/profile-pic-dark.jpg")
     img.setAttribute("alt", "Foto de Karina Sasaki de lado sorrindo, segurando a ponta do cabelo, usando óculos e camiseta cinza.")
   } else {
     html.classList.add("light");
     html.classList.remove("dark");
-    img.setAttribute("src", "assets/profile-pic-light.jpg")
+    img.setAttribute("src", "assets/profile-pic/profile-pic-light.jpg")
     img.setAttribute("alt", "Foto de rosto de Karina Sasaki, usando óculos.")
   }
 }
